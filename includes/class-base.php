73c33c99c132d3d666b4f84cf2692da89a3e2202
Daniel Post <daniel@danielpost.com>
2019-11-29 00:11:40 +0100

    Add filter to disable individual embeds

--- includes/class-base.php
+++ includes/class-base.php
@@ -39,14 +39,29 @@ class Base {
 	 * @return string
 	 */
 	public function replace_block( $block_content ) {
-		$block_content = preg_replace( '/wp-block-embed([^"\s]*)/', 'wp-block-lazy-embeds$1', $block_content );
-		$block_content = preg_replace( '/<iframe.*><\/iframe>/', $this->get_iframe_html(), $block_content );
-		$block_content = str_replace(
+		$lazy_embed_content = preg_replace( '/wp-block-embed([^"\s]*)/', 'wp-block-lazy-embeds$1', $block_content );
+		$lazy_embed_content = preg_replace( '/<iframe.*><\/iframe>/', $this->get_iframe_html(), $lazy_embed_content );
+		$lazy_embed_content = str_replace(
 			'<div class="wp-block-lazy-embeds__wrapper',
 			'<div data-lazy-embeds-' . esc_attr( $this->provider ) . '-id="' . esc_attr( $this->attributes->id ) . '" style="padding-bottom:' . esc_attr( $this->get_wrapper_spacing() ) . '%;" class="wp-block-lazy-embeds__wrapper',
-			$block_content
+			$lazy_embed_content
 		);
 
-		return $block_content;
+		/**
+		 * Filters whether the Lazy Embed should be shown.
+		 *
+		 * @param bool $enable_lazy_embed Whether the Lazy Embed will be enabled. Default true.
+		 * @param string $provider The embed provider.
+		 * @param string $id The id of the embed.
+		 * @param string $lazy_embed_content The content of the Lazy Embed block.
+		 * @param string $block_content The content of the default embed block.
+		 */
+		$lazy_embed_enabled = (bool) apply_filters( 'lazy_embeds_enable_lazy_embed', true, $this->provider, $this->attributes->id, $lazy_embed_content, $block_content );
+
+		if ( ! $lazy_embed_enabled ) {
+			return $block_content;
+		}
+
+		return $lazy_embed_content;
 	}
 }
